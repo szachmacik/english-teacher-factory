0aa928b29cfe16a1c4553fa3991946b3eb6718b4
szachmacik <maciej.koziej01@gmail.com>
2026-04-29 07:28:39 +0200

    feat: Add HOLON-META

--- client/src/const.ts
+++ client/src/const.ts
@@ -1,3 +1,5 @@
+// HOLON-META: {"purpose": "english-teacher-factory", "wiki": "32d6d069-74d6-8164-a6d5-f41c3d26ae9b"}
+
 export { COOKIE_NAME, ONE_YEAR_MS } from "@shared/const";
 
 // Redirect to Supabase OTP login page
